2d4d8d080a28e587acdf89b829bc546dc015a3a4
Kiara Atkins <atkinsk98@gmail.com>
2019-07-16 11:19:35 -0400

    added a print statement for age

--- src/co/grandcircus/GitTest.java
+++ src/co/grandcircus/GitTest.java
@@ -1,5 +1,11 @@
 package co.grandcircus;
 
 public class GitTest {
-
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Wordl!");
+		
+		int age = 21;
+		System.out.println(age);
+	}
 }
